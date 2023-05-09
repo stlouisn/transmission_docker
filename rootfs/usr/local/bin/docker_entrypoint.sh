770d15b95d05df5fd05086fffdd1f07bcf5f37fa
stlouisn <stlouisn@hotmail.com>
2023-05-08 23:02:59 -0300

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -9,28 +9,7 @@ chown -R transmission:transmission /config
 if
     [ ! -e "/config/settings.json" ]
 then
-    cat <<'EOF' > /config/settings.json
-    {
-    "bind-address-ipv4": "0.0.0.0",
-    "bind-address-ipv6": "::",
-    "blocklist-enabled": false,
-    "download-dir": "/downloads/complete",
-    "incomplete-dir": "/downloads/incomplete",
-    "incomplete-dir-enabled": true,
-    "message-level": 2,
-    "pidfile": "/config/transmission.pid",
-    "rename-partial-files": true,
-    "rpc-authentication-required": false,
-    "rpc-bind-address": "0.0.0.0",
-    "rpc-enabled": true,
-    "rpc-host-whitelist": "",
-    "rpc-host-whitelist-enabled": true,
-    "rpc-port": 9091,
-    "rpc-whitelist": "127.0.0.1,::1",
-    "rpc-whitelist-enabled": true,
-    "watch-dir-enabled": false
-    }
-    EOF
+    cp /etc/transmission/default.json /config/settings.json
 fi
 
 # Delete pre-existing PID
